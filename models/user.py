119fba2b3ec5b057baa32d0f62108079ec9857a7
Shihab <shihabunimap@gmail.com>
2023-03-12 03:36:00 +0200

    very hard

--- models/user.py
+++ models/user.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-""" Class user """
-
+"""Module for User class."""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """ Class user """
+    """Class representing a User."""
     email = ""
     password = ""
     first_name = ""
